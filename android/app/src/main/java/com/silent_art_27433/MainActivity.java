338dddea959d817462f817aac232aac009c02e22
Crowdbotics <team@crowdbotics.com>
2021-05-26 16:12:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/silent_art_27433/MainActivity.java
@@ -0,0 +1,15 @@
+package com.silent_art_27433;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "silent_art_27433";
+  }
+}
